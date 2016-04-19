f1d24f44149dfee2ef28739f8f743ec7cd728486
austin minnon and brad pizzimenti <whi7ee@gmail.com>
2016-04-19 09:15:29 -0700

    display captured name on StoryActivity

--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -1,13 +1,22 @@
 package com.epicodus.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StoryActivity extends AppCompatActivity {
-
+    public static final String TAG = StoryActivity.class.getSimpleName();
+    private TextView mNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        mNameTextView = (TextView) findViewById(R.id.nameTextView);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        mNameTextView.setText(name);
     }
 }
