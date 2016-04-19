0c4e31452d935ca6ceeb9a32801e9f9bd1e74c58
austin minnon and brad pizzimenti <brad@gennakersystems.com>
2016-04-19 09:38:29 -0700

    display place name on Story activity

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -12,19 +12,24 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mSubmitButton;
     private EditText mNameEditText;
+    private EditText mPlaceEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mNameEditText = (EditText) findViewById(R.id.nameInput);
+        mPlaceEditText = (EditText) findViewById(R.id.placeInput);
         mSubmitButton = (Button) findViewById(R.id.submit);
             mSubmitButton.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
                     String name = mNameEditText.getText().toString();
-                    Log.d(TAG, name);
+                    String place = mPlaceEditText.getText().toString();
+                    Log.d(TAG, "name: " + name);
+                    Log.d(TAG, place);
                     Intent intent = new Intent(MainActivity.this, StoryActivity.class);
                     intent.putExtra("name", name);
+                    intent.putExtra("place", place);
                     startActivity(intent);
                 }
             });
--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -8,15 +8,20 @@ import android.widget.TextView;
 public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
     private TextView mNameTextView;
+    private TextView mPlaceTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
         mNameTextView = (TextView) findViewById(R.id.nameTextView);
+        mPlaceTextView = (TextView) findViewById(R.id.placeTextView);
 
         Intent intent = getIntent();
+
         String name = intent.getStringExtra("name");
         mNameTextView.setText(name);
+        String place = intent.getStringExtra("place");
+        mPlaceTextView.setText(place);
     }
 }
