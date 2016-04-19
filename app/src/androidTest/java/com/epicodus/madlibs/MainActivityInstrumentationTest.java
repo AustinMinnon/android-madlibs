f649147362d4ff03ec0246b79707cdba59e6def8
austin minnon and brad pizzimenti <Guest@epicodus-82.local>
2016-04-19 16:35:55 -0700

    add integration tests, enters input, brings input to activity two

--- app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
@@ -1,11 +1,13 @@
 package com.epicodus.madlibs;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -19,5 +21,19 @@ public class MainActivityInstrumentationTest {
     public void validateEditText(){
         onView(withId(R.id.nameInput)).perform(typeText("Jamal"))
                 .check(matches(withText("Jamal")));
+    }
+    @Test
+    public void inputNameSentToStoryActivity(){
+        String name = "Jamal";
+        onView(withId(R.id.nameInput)).perform(typeText(name));
+        Espresso.closeSoftKeyboard();
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.submit)).perform(click());
+        onView(withId(R.id.storyText)).check(matches(withText("Jamal went to  on a  day. A   was  in front of all of the  people")));
+
     }
 }
